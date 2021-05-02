045ac13af99097b7a4842a31fc6cdeec1b277f83
DESKTOP-L5C0OCT\Анна <zhdanova.qa@gmail.com>
2021-05-02 23:41:21 +0300

    Task7

--- src/main/java/UniversityZhdanovAnna.java
+++ src/main/java/UniversityZhdanovAnna.java
@@ -1,214 +1,162 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class UniversityZhdanovAnna {
 
     public static void main(String[] args) {
-        calcProfit();
+        calcPoints();
     }
 
-    public static void calcProfit() {
-        List<TransportTemplate> transports = initTransport();
-        double totalProfit = 0.0;
-        for (int i = 0; i < transports.size(); i++) {
-            totalProfit += transports.get(i).getBuyoutSum();
-        }
-        System.out.println("Total profit from all sells: " + totalProfit);
-    }
-
-    public static List<TransportTemplate> initTransport() {
-        List<TransportTemplate> transports = new ArrayList<>();
-        List<TransportTemplate> buses = initBuses();
-        List<TransportTemplate> airplanes = initAirplanes();
-        List<TransportTemplate> ferryBoats = initFerryBoat();
-        transports.addAll(buses);
-        transports.addAll(airplanes);
-        transports.addAll(ferryBoats);
-        return transports;
-    }
-
-
-    public static List<UniversityZhdanovAnna.TransportTemplate> initBuses() {
-        UniversityZhdanovAnna.TransportTemplate bus = new UniversityZhdanovAnna.Bus(23, 56, 13, 34.0, 56.0, 17.0);
-        bus.buyoutLuggageSpace(1, 13, 14, 15, 20);
-        bus.buyoutPlace(2);
-        bus.buyoutVipPlace(1);
-        List<TransportTemplate> buses = new ArrayList<>();
-        buses.add(bus);
-        // add another buses if need
-        return buses;
-    }
-
-    public static List<UniversityZhdanovAnna.TransportTemplate> initAirplanes() {
-        UniversityZhdanovAnna.TransportTemplate airplane = new UniversityZhdanovAnna.Airplane(23, 400, 30, 134.0, 500.0, 17.0);
-        airplane.setLuggageLimit(55.0, 20.0, 30.5, 20.0);
-        airplane.buyoutLuggageSpace(1, 13, 14, 15, 20);
-        airplane.buyoutPlace(200);
-        airplane.buyoutVipPlace(23);
-        List<TransportTemplate> airplanes = new ArrayList<>();
-        airplanes.add(airplane);
-        // add another airplanes if need
-        return airplanes;
-    }
-
-    public static List<UniversityZhdanovAnna.TransportTemplate> initFerryBoat() {
-        UniversityZhdanovAnna.TransportTemplate ferryBoat = new UniversityZhdanovAnna.FerryBoat(23, 56, 13, 34.0, 56.0, 17.0);
-        ferryBoat.buyoutLuggageSpace(1, 13, 14, 15, 20);
-        ferryBoat.buyoutPlace(50);
-        ferryBoat.buyoutVipPlace(10);
-        List<TransportTemplate> ferryBoats = new ArrayList<>();
-        ferryBoats.add(ferryBoat);
-        // add another ferry boats if need
-        return ferryBoats;
-    }
-
-    public static class TransportTemplate {
-
-        int limitPlaces;
-        int limitVipPlaces;
-        int limitLuggageSpaces;
-        double costPlace;
-        double costVipPlace;
-        double costLuggage;
-        List<Place> places = new ArrayList<>();
-        List<Place> vipPlaces = new ArrayList<>();
-        List<Luggage> luggageSpaces = new ArrayList<>();
-        double limitHeight = 53.0;
-        double limitDepth = 23.0;
-        double limitWidth = 40.0;
-        double limitWeight = 25.0;
-        TransportTemplate(int numberLuggageSpaces, int numberPlaces, int numberVipPlaces, double costPlace, double costVipPlace, double costLuggage) {
-            this.limitLuggageSpaces = numberLuggageSpaces;
-            this.limitPlaces = numberPlaces;
-            this.limitVipPlaces = numberVipPlaces;
-            this.costPlace = costPlace;
-            this.costVipPlace = costVipPlace;
-            this.costLuggage = costLuggage;
-        }
-
-        public void setLuggageLimit(double height, double depth, double width, double weight) {
-            this.limitHeight = height;
-            this.limitDepth = depth;
-            this.limitWidth = width;
-            this.limitWeight = weight;
-        }
-
-        public boolean checkLuggageLimit(double height, double depth, double width, double weight) {
-
-            if (height > limitHeight) {
-                System.out.println("Wrong height");
-                return false;
+    public static void calcPoints() {
+        Map<Student, Task[]> objs = initObjects();
+        for (Map.Entry<Student, Task[]> entry : objs.entrySet()) {
+            Task[] tasks = entry.getValue();
+            int countPoints = 0;
+            for (int i = 0; i < tasks.length; i++) {
+                countPoints += tasks[i].countPoints;
             }
-            if (depth > limitDepth) {
-                System.out.println("Wrong depth");
-                return false;
-            }
-            if (width > limitWidth) {
-                System.out.println("Wrong width");
-                return false;
-            }
-            if (weight > limitWeight) {
-                System.out.println("Wrong weight");
-                return false;
-            }
-            return true;
-        }
-
-        public void buyoutLuggageSpace(int count, double height, double depth, double width, double weight) {
-            if (limitLuggageSpaces - luggageSpaces.size() >= count) {
-                for (int i = 0; i < count; i++) {
-                    if (!checkLuggageLimit(height, depth, width, weight)) {
-                        continue;
-                    }
-                    UniversityZhdanovAnna.TransportTemplate.Luggage newLuggage = new Luggage(costLuggage, height, depth, width, weight);
-                    luggageSpaces.add(newLuggage);
-                }
-            } else {
-                System.out.println("You can't buy more luggage spaces");
-            }
-        }
-
-        public void buyoutPlace(int count) {
-            if (limitPlaces - places.size() >= count) {
-                for (int i = 0; i < count; i++) {
-                    UniversityZhdanovAnna.TransportTemplate.Place newPlace = new Place(costPlace);
-                    places.add(newPlace);
-                }
-            } else {
-                System.out.println("You can't buy more places");
-            }
-        }
-
-        public void buyoutVipPlace(int count) {
-            if (limitVipPlaces - vipPlaces.size() >= count) {
-                for (int i = 0; i < count; i++) {
-                    UniversityZhdanovAnna.TransportTemplate.Place newVipPlace = new Place(costVipPlace);
-                    vipPlaces.add(newVipPlace);
-                }
-            } else {
-                System.out.println("You can't buy more vip places");
-            }
-        }
-
-        public double getBuyoutSum() {
-            int commonWight = 0;
-            double sum = 0.0;
-            for (int i = 0; i < luggageSpaces.size(); i++) {
-                commonWight += luggageSpaces.get(i).weight;
-                sum += commonWight * costLuggage;
-            }
-            sum += places.size() * costPlace + vipPlaces.size() * costVipPlace;
-            return sum;
-        }
-
-        private static class Luggage {
-            double cost;
-            double height;
-            double depth;
-            double width;
-            double weight;
-
-            Luggage(double cost, double height, double depth, double width, double weight) {
-                this.cost = cost;
-                this.height = height;
-                this.weight = weight;
-                this.depth = depth;
-                this.width = width;
-            }
-
-            public double getWeight() {
-                return this.weight;
-            }
-        }
-
-        private static class Place {
-            double cost;
-
-            Place(double defaultCost) {
-                this.cost = defaultCost;
-            }
-
+            System.out.println("Student " + entry.getKey().surname + " " + entry.getKey().name + " have points : " + countPoints);
         }
     }
 
-    public static class Bus extends TransportTemplate {
+    public static Map<Student, Task[]> initObjects() {
+        Map<Student, Task[]> collection = new HashMap<Student, Task[]>();
+        for (int i = 0; i < 7; i++) {
+            Student student = getStudent();
+            Practice practice = getPractice();
+            Lecture lecture = getLecture();
+            Task[] tasks = {practice, lecture};
+            collection.put(student, tasks);
+        }
 
-        Bus(int numberLuggageSpaces, int numberPlaces, int numberVipPlaces, double costPlace, double costVipPlace, double costLuggage) {
-            super(numberLuggageSpaces, numberPlaces, numberVipPlaces, costPlace, costVipPlace, costLuggage);
+        return collection;
+    }
+
+    public static Student getStudent() {
+        String[] maleSurnames = {"Ivanov", "Petrov", "Sidorov", "Arshavin", "Svetoch"};
+        String[] maleNames = {"Andrei", "Ivan", "Igor", "Viktor", "Dmitry"};
+        String[] femaleSurnames = {"Ivanova", "Petrova", "Sidorova", "Arshavina", "Svetocheva"};
+        String[] femaleNames = {"Kate", "Yulia", "Sasha", "Tania"};
+        int choiceType = getRandomInt(1, 2);
+        int choiceAge = getRandomInt(16, 28);
+        int choiceName;
+        int choiceSurname;
+        Student result = null;
+        switch (choiceType) {
+            case 1:
+                choiceName = getRandomInt(0, maleNames.length-1);
+                choiceSurname = getRandomInt(0, maleSurnames.length-1);
+                result = new BoyStudent(maleSurnames[choiceSurname], maleNames[choiceName], choiceAge);
+            case 2:
+                choiceName = getRandomInt(0, femaleNames.length-1);
+                choiceSurname = getRandomInt(0, femaleSurnames.length-1);
+                result = new GirlStudent(femaleSurnames[choiceSurname], femaleNames[choiceName], choiceAge);
+            default:
+                break;
+        }
+        return result;
+    }
+
+    public static Lecture getLecture() {
+        int choicePoints = getRandomInt(1, 10);
+        Lecture lecture = new Lecture("Lecture", choicePoints);
+        return lecture;
+    }
+
+    public static Practice getPractice() {
+        int choicePoints = getRandomInt(10, 100);
+        int choiceLevel = getRandomInt(1, 3);
+        Practice result = new Practice("Practice", choicePoints, choiceLevel);
+        return result;
+    }
+
+    public static int getRandomInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+
+    public static class Student {
+        String name;
+        String surname;
+        int age;
+
+        Student(String surname, String name, int age) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
         }
     }
 
-    public static class Airplane extends TransportTemplate {
+    public static class Task {
+        String typeTask;
+        int countPoints;
 
-        Airplane(int numberLuggageSpaces, int numberPlaces, int numberVipPlaces, double costPlace, double costVipPlace, double costLuggage) {
-            super(numberLuggageSpaces, numberPlaces, numberVipPlaces, costPlace, costVipPlace, costLuggage);
+        Task(String typeTask, int countPoints) {
+            this.typeTask = typeTask;
+            this.countPoints = countPoints;
         }
     }
 
-    public static class FerryBoat extends TransportTemplate {
+    public static class Practice extends Task {
 
-        FerryBoat(int numberLuggageSpaces, int numberPlaces, int numberVipPlaces, double costPlace, double costVipPlace, double costLuggage) {
-            super(numberLuggageSpaces, numberPlaces, numberVipPlaces, costPlace, costVipPlace, costLuggage);
+        public static Level level;
+
+        Practice(String typeTask, int countPoints, int level) {
+            super(typeTask, countPoints);
+            setLevel(level);
+        }
+
+        public void setLevel(int l) {
+            switch (l) {
+                case 1:
+                    level = Level.Easy;
+                case 2:
+                    level = Level.Medium;
+                case 3:
+                    level = Level.Hard;
+                default:
+                    this.countPoints = this.countPoints * level.getNumVal();
+                    break;
+            }
+        }
+
+        public enum Level {
+            Easy(1), Medium(2), Hard(3),
+            ;
+
+            private final int numVal;
+
+            Level(int numVal) {
+                this.numVal = numVal;
+            }
+
+            public int getNumVal() {
+                return numVal;
+            }
+        }
+    }
+
+    public static class Lecture extends Task {
+
+        Lecture(String typeTask, int countPoints) {
+            super(typeTask, countPoints);
+        }
+    }
+
+
+    public static class GirlStudent extends Student {
+
+        GirlStudent(String surname, String name, int age) {
+            super(surname, name, age);
+        }
+    }
+
+    public static class BoyStudent extends Student {
+
+        BoyStudent(String surname, String name, int age) {
+            super(surname, name, age);
         }
     }
 }
